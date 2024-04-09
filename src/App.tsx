315957390ca0bc83b4f3d82c7190d9ac76410c5e
unknown <sashakotik450697@gmail.com>
2024-04-09 21:07:32 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// import './App.css'
+import CategoryList from "./components/Category/categoryList.tsx";
+import {Route, Routes} from "react-router-dom";
+import NotFoundPage from "./Pages/NotFoundPage.tsx";
+import AdminLayout from "./components/containers/admin/AdminLayout.tsx";
+
+const App = () => {
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<AdminLayout/>}>
+                    <Route index element={<CategoryList/>}/>
+                    {/*<Route path="users" element={<UsersTasks />} />*/}
+                    {/*<Route path="tasks" element={<TasksPage />} />*/}
+                </Route>
+
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
+        </>
+    )
+}
+
+export default App
---
+++ src/Pages/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+const NotFoundPage = () => {
+    return (
+        <section className="mx-auto flex min-h-screen items-center justify-center">
+            <div className="container flex flex-col items-center">
+                <div className="flex flex-col gap-6 text-center">
+                    <h2 className="text-9xl font-extrabold text-gray-600 dark:text-gray-100">
+                        <span className="sr-only">Error</span>404
+                    </h2>
+                    <p className="text-2xl md:text-3xl dark:text-gray-300">Sorry, we couldn't find this page.</p>
+                    <Link to={"/"} className="underline">Go to home</Link>
+                </div>
+        </div>
+        </section>
+    );
+};
+export default NotFoundPage;
